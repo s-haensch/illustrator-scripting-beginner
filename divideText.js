6fdd4413b79d2e0c7d158502d54ee5fd05a187c9
Steffen Hänsch <steffen.haensch@web.de>
2017-05-03 15:18:08 +0200

    final script.

--- divideText.js
+++ divideText.js
@@ -1,19 +1,26 @@
 var doc = app.activeDocument;
 var selection = doc.selection;
 
-for (s = 0; s < selection.length; s++) {
-  currentObject = selection[s];
+for (i = 0; i < selection.length; i++) {
+  currentObject = selection[i];
 
   if (currentObject instanceof TextFrame) {
-    // get all lines included in the text frame
     var lines = currentObject.lines;
 
-    // loop through all the lines
+    // get line height
+    var lineHeight = currentObject.textRange.characterAttributes.leading;
+
     for (l = 0; l < lines.length; l++) {
-      // create a new empty text frame
       var newText = doc.textFrames.add();
-      // duplicate the content of the current line to the new text frame
+
+      // set position of new text frame
+      newText.left = currentObject.left;
+      newText.top = currentObject.top - (l * lineHeight);
+
       lines[l].duplicate(newText.textRange, ElementPlacement.PLACEATBEGINNING);
     }
+
+    // after we've copied all the lines, remove the original text frame
+    currentObject.remove();
   }
 }
